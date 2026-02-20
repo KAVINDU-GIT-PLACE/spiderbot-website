5ac8d24f6b3d704c561a4563b17e5bebffd15c01
KAVINDU-GIT-PLACE <fernandopwksk.24@uom.lk>
2026-02-20 21:31:42 +0530

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 document.getElementById("lastUpdate").innerText =
   new Date().toLocaleTimeString();
+let coHistory = [];
+let startTime = Date.now();
+
+database.ref("hexapod").on("value", (snapshot) => {
+
+  const data = snapshot.val();
+  if(data){
+
+    coHistory.push(data.co);
+
+    if(coHistory.length > 100){
+      coHistory.shift();
+    }
+
+    const max = Math.max(...coHistory);
+    const avg = (coHistory.reduce((a,b)=>a+b,0) / coHistory.length).toFixed(0);
+
+    document.getElementById("maxCo").innerText = max;
+    document.getElementById("avgCo").innerText = avg;
+
+    const seconds = Math.floor((Date.now() - startTime)/1000);
+    document.getElementById("uptime").innerText = seconds + " sec";
+  }
+
+});
